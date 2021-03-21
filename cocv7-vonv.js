cfbe64cd39d7637c2732c237da4bdf201a0f7386
Trouver Objet Caché <44206935+vonv@users.noreply.github.com>
2021-03-21 23:49:10 +0100

    Add files via upload

---
+++ cocv7-vonv.js
@@ -0,0 +1,49 @@
+function collapse(toggleId) {
+  let target = document.getElementById(toggleId);
+  if (target) {
+    target.click();
+  }
+}
+
+Hooks.on('ready', () => {
+  // Importer Control Menu
+
+  game.settings.register('CoC7', 'artworkMainFont',{
+    name: 'SETTINGS.ArtworkMainFont',
+    // hint: 'SETTINGS.ArtworkMainFontHint',
+    scope: 'world',
+    config: true,
+    default: 'url(\'./modules/cocv7-vonv/fonts/mailart-rubberstamp.ttf\') format(\'truetype\')',
+    type: String
+  });
+
+  game.settings.register('CoC7', 'artworkMainFontBold',{
+    name: 'SETTINGS.ArtworkMainFontBold',
+    // hint: 'SETTINGS.ArtworkMainFontHint',
+    scope: 'world',
+    config: true,
+    default: 'url(\'./modules/cocv7-vonv/fonts/mailart-rubberstamp.ttf\') format(\'truetype\')',
+    type: String
+  });
+
+  game.settings.register('CoC7', 'artworkMainFontSize',{
+    name: 'SETTINGS.ArtworkMainFontSize',
+    // hint: 'SETTINGS.ArtworkMainFontSizeHint',
+    scope: 'world',
+    config: true,
+    default: 20,
+    type: Number
+  });
+
+  game.settings.register('CoC7', 'artworkFrontColor',{
+    name: 'SETTINGS.ArtworkFrontColor',
+    hint: 'SETTINGS.ArtworkFrontColorHint',
+    scope: 'world',
+    config: true,
+    default: 'rgba(43,55,83,1)',
+    type: String
+  });
+  
+  collapse("bar-toggle");
+
+});
